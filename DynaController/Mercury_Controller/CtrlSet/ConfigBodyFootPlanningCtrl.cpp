4a2ab7500c5e3de50ac17bfea557bc6c8bdc04b6
hcrl <donghyunkim>
2018-08-11 20:30:28 -0500

    near to the optimal parameter

--- DynaController/Mercury_Controller/CtrlSet/ConfigBodyFootPlanningCtrl.cpp
+++ DynaController/Mercury_Controller/CtrlSet/ConfigBodyFootPlanningCtrl.cpp
@@ -189,7 +189,8 @@ void ConfigBodyFootPlanningCtrl::_Replanning(dynacore::Vect3 & target_loc){
         // com_vel[i] = sp_->average_vel_[i]; 
         // com_vel[i] = sp_->est_CoM_vel_[i]; 
 
-         com_pos[i] = sp_->est_mocap_body_pos_[i] + body_pt_offset_[i];
+         // com_pos[i] = sp_->est_mocap_body_pos_[i] + body_pt_offset_[i];
+        com_pos[i] = sp_->jjpos_body_pos_[i] + body_pt_offset_[i];
          com_vel[i] = sp_->est_mocap_body_vel_[i]; 
     }
     printf("planning com state: %f, %f, %f, %f\n",
