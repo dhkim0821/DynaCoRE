90a046aa744e519f903f30419280d66905b9b84d
hcrl <donghyunkim>
2018-08-30 12:41:21 -0500

    adding wbc gains are better

--- DynaController/Mercury_Controller/MoCapManager.cpp
+++ DynaController/Mercury_Controller/MoCapManager.cpp
@@ -28,7 +28,7 @@ MoCapManager::MoCapManager(const RobotSystem* robot):dynacore_pThread(),
     led_pos_data_.setZero();
     led_kin_data_.setZero();
     DataManager::GetDataManager()->RegisterData(&led_pos_data_, DYN_VEC, "LED_Pos", 3*NUM_MARKERS);
-    DataManager::GetDataManager()->RegisterData(&led_kin_data_, DYN_VEC, "LED_Kin_Pos", 3*NUM_MARKERS);
+    // DataManager::GetDataManager()->RegisterData(&led_kin_data_, DYN_VEC, "LED_Kin_Pos", 3*NUM_MARKERS);
     // DataManager::GetDataManager()->RegisterData(&led_pos_raw_data_, DYN_VEC, "LED_Pos_Raw", 3*NUM_MARKERS);
 
     sp_ = Mercury_StateProvider::getStateProvider();
